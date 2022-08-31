3816fbd13f3ad864528c2bbc6b3331be32b21b0e
KirillStrukov <102173578+KirillStrukov@users.noreply.github.com>
2022-08-31 04:13:30 +0300

    fix all

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
+// ----------------------------------------------------------------------
+let modal = document.getElementById('myModal');
+
+
+// let img = document.getElementById('myImg');
+
+let img = document.querySelectorAll('.photo_gallery img');
+img = Array.from(img);
+
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+img.forEach(i => i.addEventListener('click', (event)=>{
+
+    modal.style.display = "block";
+    modalImg.src = event.target.src;
+	console.log(event.target)
+    captionText.innerHTML = this.alt;
+
+})) ;
+
+let span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
